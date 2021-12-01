f3c268a33f5ff360d5c116cfe143d74be6e23532
Benjamin Watson <benjaminianwatson@gmail.com>
2021-12-01 07:54:23 +0000

    Made quotes consistent

--- template.py
+++ template.py
@@ -6,8 +6,8 @@ def part_2(data):
     return None
 
 
-if __name__ == '__main__':
-    with open('day_xx_input.txt', 'r') as f:
+if __name__ == "__main__":
+    with open("day_xx_input.txt", "r") as f:
         inp = f.readlines()
         print("Part 1: " + str(part_1(inp)))
         print("Part 2: " + str(part_2(inp)))
